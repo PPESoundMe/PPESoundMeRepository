019a5bb9491724430a8b45d63b97b14e4800ee71
Zhenhao LIN <vincent.lin@edu.ece.fr>
2018-03-29 10:37:55 +0200

    Reserver+affichage_reservations
    La page html de la reservation + la page d'affichage des reservations selon utilisateurs

--- planning.php
+++ planning.php
@@ -1,8 +1,6 @@
 <html>
 <head>
 <link href="../css/calendar.css" type="text/css" rel="stylesheet" />
-<link rel="shortcut icon" href="photos/logo_onglet.ico">
-<title> Planning </title>
 </head>
 <body>
 <?php
